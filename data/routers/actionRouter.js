b99022612ed30b1571df7a614cf8d7cc6169dca3
Oliver-Strange <preston.burton@gmail.com>
2019-06-07 08:39:39 -0700

    index requires server and listens to port 4000, set up router files, adds comments for planning

---
+++ data/routers/actionRouter.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const ProjectDb = require("../helpers/projectModel");
+const ActionDb = require("../helpers/actionModel");
+
+const router = express.Router();
+
+// GET actions
+
+// GET actions by Id
+
+// POST action
+
+// PUT action
+
+// DELETE action
+
+module.exports = router;
---
+++ data/routers/projectRouter.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const ProjectDb = require("../helpers/projectModel");
+const ActionDb = require("../helpers/actionModel");
+
+const router = express.Router();
+
+// GET projects
+
+// GET projects by Id
+
+// POST project
+
+// PUT project
+
+// DELETE project
+
+module.exports = router;
